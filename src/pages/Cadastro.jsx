aa89af31e6199a4c7c7ac102890231781cb6ab99
Arthur Cavalcante <linsarthur02@gmail.com>
2025-06-10 11:16:35 -0300

    finalizado cadastro

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -3,8 +3,8 @@ import casinha from "../assets/casinha.png";
 const Cadastro = () => {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
-        <div className="border-[1px] border-gray-500/20 shadow pt-[40px] pb-[40px] px-[28px]">
+      <div className="flex justify-center items-center h-screen flex-col relative">
+        <div className="border-[1px] border-gray-500/20 rounded shadow pt-[40px] pb-[40px] px-[28px] ">
           <div className="flex items-center gap-[75px] pr-[124px]">
             <div className="pb-[50px] ">
               <box-icon name="left-arrow-alt" color="#e04300"></box-icon>
@@ -14,48 +14,55 @@ const Cadastro = () => {
                 <img src={casinha} alt="" className="p-[6px]" />
               </div>
               <div>
-                <h2 className="text-[#E04300] font-bold ">Aluga Web</h2>
+                <h2 className="text-[#DF4300] font-bold ">Aluga Web</h2>
               </div>
             </div>
           </div>
-          <Form layout="vertical" >
-            <Form.Item
-              label="Nome"
-              name="usuario_nome"
-              rules={[{ required: true, message: "Campo obrigatorio!" }]}
-            >
-              <Input
-                placeholder="Seu nome aqui"
-                className="h-[54px] w-[344px]"
-                variant="filled"
-              />
-            </Form.Item>
+          <div>
+            <Form layout="vertical">
+              <Form.Item
+                label="Nome"
+                name="usuario_nome"
+                rules={[{ required: true, message: "Campo obrigatorio!" }]}
+              >
+                <Input
+                  placeholder="Seu nome aqui"
+                  className="h-[54px] w-[344px]"
+                  variant="filled"
+                />
+              </Form.Item>
 
-            <Form.Item
-              label="Email"
-              name="usuario_email"
-              rules={[{ required: true, message: "Campo obrigatorio!" }]}
-            >
-              <Input
-                placeholder="email@gmail.com"
-                className="h-[54px]"
-                variant="filled"
-              />
-            </Form.Item>
+              <Form.Item
+                label="Email"
+                name="usuario_email"
+                rules={[{ required: true, message: "Campo obrigatorio!" }]}
+              >
+                <Input
+                  placeholder="email@gmail.com"
+                  className="h-[54px]"
+                  variant="filled"
+                />
+              </Form.Item>
 
-            <Form.Item
-              label="Senha"
-              name="usuario_senha"
-              rules={[{ required: true, message: "Campo obrigatorio!" }]}
-            >
-              <Input
-                placeholder="Senha aqui"
-                className="h-[54px]"
-                variant="filled"
-              />
-            </Form.Item>
-            <Button color="primary" variant="solid" className="w-full">Enviar</Button>
-          </Form>
+              <Form.Item
+                label="Senha"
+                name="usuario_senha"
+                rules={[{ required: true, message: "Campo obrigatorio!" }]}
+              >
+                <Input
+                  placeholder="Senha aqui"
+                  className="h-[54px]"
+                  variant="filled"
+                />
+              </Form.Item>
+              <Button color="primary" variant="solid" className="w-full">
+                Enviar
+              </Button>
+            </Form>
+          </div>
+        </div>
+        <div className="absolute bottom-10 text-[#E04300] font-bold">
+          <h6>Aluga web, todos os direitos reservados.</h6>
         </div>
       </div>
     </>
