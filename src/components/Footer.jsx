15cf1881f5eb0b34837355eb8bdd92a6f045d16a
Mcr-Maria <ceissinha@gmail.com>
2025-06-11 16:15:02 -0300

    fix: correcao de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,26 @@
-import rodape from '../assets/casinha.png';
+import rodape from "../assets/casinha.png";
 
 const Footer = () => {
-    return ( 
+    return (
         <>
-        <div className='bg-amber-700'>
-            <img
-            src=''
-            alt=''
-            className=''/>
+            <div className="bg-[#DF4300] flex justify-between h-[100px] mt-[647px]">
+                <div className=" flex items-center">
+                    <div className="bg-[#FFFFFF] rounded-[4px] w-[36px] h-[36px] mt-[38px] ml-[56px] mb-[38px] flex justify-center items-center">
+                        <img
+                            src={rodape}
+                            alt="Rodapé"
+                            className=" " />
+                    </div>
 
-        </div>
+                    <h3 className="text-[#FFFFFF] ml-[10px] font-bold">Aluga Web</h3>
+                </div>
+
+
+                <div>
+                    <h3 className="text-[#FFFFFF] font-bold mt-[38px] mr-[50px] ">Todos os direitos reservados.</h3>
+                </div>
+            </div>
+
         </>
 
     );
--- src/pages/Testes.jsx
+++ src/pages/Testes.jsx
@@ -1,27 +1,9 @@
 import Aba from "../components/Aba";
-import rodape from "../assets/casinhalaranja.png";
+
 
 const Testes = () => {
   return (
     <>
-      <div className="bg-[#DF4300] flex justify-between h-[100px] mt-[647px]">
-        <div className=" flex items-center">
-          <div className="bg-[#FFFFFF] rounded-[4px] w-[36px] h-[36px] mt-[38px] ml-[56px] mb-[38px] flex justify-center items-center">
-            <img
-              src={rodape}
-              alt="Rodapé"
-              className=" " />
-          </div>
-
-          <h3 className="text-[#FFFFFF] ml-[10px] font-bold">Aluga Web</h3>
-        </div>
-
-
-        <div>
-          <h3 className="text-[#FFFFFF] font-bold mt-[38px] mr-[50px] ">Todos os direitos reservados.</h3>
-        </div>
-      </div>
-
     </>
   );
 };
