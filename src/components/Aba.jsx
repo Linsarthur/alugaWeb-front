8033b315e1ce33e8da7079b442905af5902930b4
Arthur Cavalcante <linsarthur02@gmail.com>
2025-06-11 09:30:22 -0300

    fix

--- src/components/Aba.jsx
+++ src/components/Aba.jsx
@@ -1,7 +1,7 @@
 const Aba = () => {
   return (
     <>
-      <div className=" text-white m-10">
+      <div className=" text-white">
         <button className="py-[16px] px-[80px] bg-[#00000026] text-[#595959] rounded-tl-2xl">Comprar</button>
         <button className="py-[16px] px-[80px] bg-[#E04300] text-white rounded-tr-2xl">Alugar</button>
       </div>
