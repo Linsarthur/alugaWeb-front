3dff5812ba153b1e48078fadef4e573721190def
Ana nobrega <anakarolinefrnobrega@gmail.com>
2025-06-11 13:43:37 -0300

    feat:atualização da home com resposividade

--- src/routes/Paths.jsx
+++ src/routes/Paths.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Cadastro from "../pages/Cadastro";
 import Testes from "../pages/testes";
+import Home from "../pages/home";
 
 const Paths = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
           {/* <Route path="/login" element={<Login />} /> */}
           <Route path="/cadastro" element={<Cadastro />} />
           {/* <Route path="/pesquisa" element={<Pesquisa />} /> */}
