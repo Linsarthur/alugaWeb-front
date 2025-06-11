fbd540bb56d3234682ea5e0a53059f5cd002ad39
Ana nobrega <anakarolinefrnobrega@gmail.com>
2025-06-10 21:40:20 -0300

    criação da home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
+import React from 'react';
+import mulherImg from '../assets/mulher.png'; // ajuste o caminho conforme o nome da imagem
+
 import pin from '../assets/pin.png';
 import mulher from '../assets/mulher.png';
 import { useNavigate } from 'react-router';
 
-
 const Home = () => {
-
     // const navigate = useNavigate();
-
     return (
         <>
             <div className="h-screen flex  justify-around bg-white">
@@ -15,15 +15,12 @@ const Home = () => {
                         <h1 className="text-[36px] font-extrabold leading-[100%] text-[#1E1E1E]">
                             Onde você quer morar
                         </h1>
-
                         <div className="flex items-center w-[606px]  border-[3px] border-[#E04300] rounded-full overflow-hidden p-[11px] mt-[30px]">
                             <img src={pin} alt="Ícone de localização" className="w-[23px] ml-[29px]" />
-
                             <input
                                 type="text"
                                 className="flex-1 h-full outline-none text-[#1E1E1E] text-base"
                             />
-
                             <button
                                 className="w-[116px] h-[54px] bg-[#E04300] text-white font-bold rounded-full cursor-pointer"
                                 // onClick={() => navigate('/')}
@@ -32,7 +29,6 @@ const Home = () => {
                             </button>
                         </div>
                     </div>
-
                     <div className="relative w-full h-screen overflow-visible ml-20">
                         <img
                             src={mulher}
@@ -41,12 +37,9 @@ const Home = () => {
                         />
                     </div>
 
-
                 </main>
-
             </div>
         </>
     );
 }
-
 export default Home;
