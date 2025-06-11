e223b3fb88a385e6ee3649dca708d64b764f7e04
Mcr-Maria <ceissinha@gmail.com>
2025-06-11 13:30:25 -0300

    footer inicial

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import rodape from '../assets/casinha.png';
+
+const Footer = () => {
+    return ( 
+        <>
+        <div className='bg-amber-700'>
+            <img
+            src=''
+            alt=''
+            className=''/>
+
+        </div>
+        </>
+
+    );
+}
+
+export default Footer;
