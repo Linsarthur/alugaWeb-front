312d7b09853d26193b9d5af7f0160a7a913831ef
Arthur Cavalcante <linsarthur02@gmail.com>
2025-06-12 12:47:19 -0300

    fix routes

--- src/routes/Paths.jsx
+++ src/routes/Paths.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import Cards from "../components/Cards";
 import Cadastro from "../pages/Cadastro";
 import Home from "../pages/Home";
 
@@ -12,7 +11,7 @@ const Paths = () => {
           {/* <Route path="/login" element={<Login />} /> */}
           <Route path="/cadastro" element={<Cadastro />} />
           {/* <Route path="/pesquisa" element={<Pesquisa />} /> */}
-          <Route path="/testes" element={<Cards />} />
+          {/* <Route path="/testes" element={<Cards />} /> */}
         </Routes>
       </BrowserRouter>
     </>
