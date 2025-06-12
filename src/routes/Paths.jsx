0844e59ae0a7e6d9ca9fe12a5ad33f25bcbf12f9
Arthur Cavalcante <linsarthur02@gmail.com>
2025-06-12 09:15:37 -0300

    card parcialmente feito

--- src/routes/Paths.jsx
+++ src/routes/Paths.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
+import Cards from "../components/Cards";
 import Cadastro from "../pages/Cadastro";
 import Home from "../pages/Home";
-import Testes from "../pages/testes";
-
 
 const Paths = () => {
   return (
@@ -13,7 +12,7 @@ const Paths = () => {
           {/* <Route path="/login" element={<Login />} /> */}
           <Route path="/cadastro" element={<Cadastro />} />
           {/* <Route path="/pesquisa" element={<Pesquisa />} /> */}
-          <Route path="/testes" element={<Testes />} />
+          <Route path="/testes" element={<Cards />} />
         </Routes>
       </BrowserRouter>
     </>
