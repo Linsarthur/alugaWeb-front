06f757c5633f940c38b55e582c4dc5bd10d74af8
Arthur Cavalcante <linsarthur02@gmail.com>
2025-06-15 16:18:33 -0300

    fix: fixing cadastro page

--- src/components/FormCadastro.jsx
+++ src/components/FormCadastro.jsx
@@ -1,48 +1,59 @@
 import { Button, Form, Input } from "antd";
 
 const FormCadastro = () => {
-    return <>
-     <Form layout="vertical">
-              <Form.Item
-                label="Nome"
-                name="usuario_nome"
-                rules={[{ required: true, message: "Campo obrigatorio!" }]}
-              >
-                <Input
-                  placeholder="Seu nome aqui"
-                  className="h-[54px] w-[344px]"
-                  variant="filled"
-                />
-              </Form.Item>
+  return (
+    <>
+      <div className="">
+        <Form layout="vertical" className="w-[344px]">
+          <Form.Item
+            className="font-bold"
+            label="Nome"
+            name="usuario_nome"
+            rules={[{ required: true, message: "Campo obrigatorio!" }]}
+          >
+            <Input
+              placeholder="Seu nome aqui"
+              className="h-[54px] w-full bg-"
+              variant="filled"
+            />
+          </Form.Item>
 
-              <Form.Item
-                label="Email"
-                name="usuario_email"
-                rules={[{ required: true, message: "Campo obrigatorio!" }]}
-              >
-                <Input
-                  placeholder="email@gmail.com"
-                  className="h-[54px]"
-                  variant="filled"
-                />
-              </Form.Item>
+          <Form.Item
+            className="font-bold"
+            label="Email"
+            name="usuario_email"
+            rules={[{ required: true, message: "Campo obrigatorio!" }]}
+          >
+            <Input
+              placeholder="email@gmail.com"
+              className="h-[54px]"
+              variant="filled"
+            />
+          </Form.Item>
 
-              <Form.Item
-                label="Senha"
-                name="usuario_senha"
-                rules={[{ required: true, message: "Campo obrigatorio!" }]}
-              >
-                <Input
-                  placeholder="Senha aqui"
-                  className="h-[54px]"
-                  variant="filled"
-                />
-              </Form.Item>
-              <Button color="primary" variant="solid" className="w-full">
-                Enviar
-              </Button>
-            </Form>
+          <Form.Item
+            className="font-bold"
+            label="Senha"
+            name="usuario_senha"
+            rules={[{ required: true, message: "Campo obrigatorio!" }]}
+          >
+            <Input
+              placeholder="Senha aqui"
+              className="h-[54px]"
+              variant="filled"
+            />
+          </Form.Item>
+          <Button
+            color="primary"
+            variant="solid"
+            className="!py-[18px] w-full"
+          >
+            Enviar
+          </Button>
+        </Form>
+      </div>
     </>
-}
- 
+  );
+};
+
 export default FormCadastro;
