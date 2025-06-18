d9556f3e3c0875f9d0021ac591c05b6cf4e28d40
vitoriaAlbuquerque07 <vitoriam.albuquerque00@gmail.com>
2025-06-18 14:13:53 -0300

    fix

--- src/services/index.js
+++ src/services/index.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const AXIOS = axios.create({
     //baseURL: "http://localhost:8000"
-    baseURL: "https://alugaweb-back.onrender.com/"
+    baseURL: "https://alugaweb-back.onrender.com"
 })
 
