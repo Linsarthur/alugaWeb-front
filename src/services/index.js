b05b81c6049f6d38119396effbbb3fb99c77a9f3
Arthur Cavalcante <linsarthur02@gmail.com>
2025-06-18 13:51:46 -0300

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const AXIOS = axios.create({
-    baseURL: "http://localhost:8000"
+    //baseURL: "http://localhost:8000"
+    baseURL: "https://alugaweb-back.onrender.com"
 })
